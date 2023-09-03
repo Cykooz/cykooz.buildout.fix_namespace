f385c41771cfa54b239e23aa9cb0236212b0b606
Kirill Kuzminykh <cykooz@gmail.com>
2023-09-03 16:44:02 +0300

    - Added fix that creates ``namespace_packages.txt`` for some packages with   native namespaces which doesn't have it. - Removed fix of content of namespace ``__init__.py`` file.

--- src/cykooz/buildout/fixnamespace/runtests.py
+++ src/cykooz/buildout/fixnamespace/runtests.py
@@ -4,17 +4,19 @@
 :Date: 14.04.2020
 """
 
-
 def runtests():
     import sys
     import pytest
-    from os import environ
-    from os.path import dirname, join
-    cfg_path = join(dirname(dirname(dirname(dirname(__file__)))), 'setup.cfg')
+    from pathlib import Path
+    root_dir_path = Path(__file__).parent / '..' / '..' / '..' / '..'
+    cfg_path = root_dir_path / 'src' / 'setup.cfg'
 
     args = sys.argv[1:]
     if not args or args[0].startswith('-'):
         args += ['--pyargs', 'cykooz.buildout.fixnamespace']
-    args = ['-c', cfg_path] + args
-    environ['IS_TESTING'] = 'True'
-    pytest.main(args)
+    args = [
+               '-c', str(cfg_path),
+               '--rootdir', str(root_dir_path),
+           ] + args
+
+    return pytest.main(args)
